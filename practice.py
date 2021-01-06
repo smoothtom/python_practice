dc5c422b461ace914ba0ce781516126a026da398
smoothtom <t-lauth@web.de>
2021-01-06 12:29:28 +0100

    first commit

---
+++ practice.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+# Practice Python - Beginner Python exercises 
+
+def character_input():
+    name = input("Please Enter your name: ")
+    age = int(input("Please Enter your age: "))
+    year = (2020-age)+100
+    print(name, "will be 100 in year", year)
+character_input()
+
+def odd_or_even():
+    num = int(input("Please Enter a number to check: "))
+    check = int(input("Please Enter number to divide: "))
+    if (num % 4) == 0:
+        print(num, "is a multiple of 4")
+    elif (num % 2) == 0:
+        print(num, "is even")
+    else:
+        print(num, "is odd")
+
+    if (num % check) == 0:
+        print(num, "divides evenly by", check)
+    else:
+        print(num, "does not divide evenly by", check)
+# odd_or_even()
+
+def list_less_than_ten():
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
