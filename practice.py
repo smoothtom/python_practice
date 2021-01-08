25d815e08d99db46d279bef4d5250b87216b2972
smoothtom <t-lauth@web.de>
2021-01-09 00:27:43 +0100

    change the function of primality number

--- practice.py
+++ practice.py
@@ -121,12 +121,9 @@ def check_primality_functions():
   if user > 1:
     for i in range(2,user):
       if (user % i) == 0:
-        print("this is a prime number")
+        print("this isn't a prime number")
         break
     else:
-      print("this isn't a prime number")
+      print("this is a prime number")
 check_primality_functions()
-
-
-
 
